84c0dfaddd944769658ff8f31d2d4f7f29ee042e
LisieuxS <a01207648@itesm.mx>
2022-02-23 10:14:23 -0600

    add validation

--- MvcMovie/MvcMovie/Models/SeedData.cs
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -7,7 +7,7 @@ using System.Linq;
 namespace MvcMovie.Models
 {
     public static class SeedData
-    {
+    {   
         public static void Initialize(IServiceProvider serviceProvider)
         {
             using (var context = new MvcMovieContext(
